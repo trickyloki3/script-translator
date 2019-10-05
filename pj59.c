783a68b4d0b1c26d58c42e8fb2edaa14a274fb14
trickyloki3 <tricky.loki3@gmail.com>
2019-10-05 08:49:10 -0400

    * Initial pj59.

--- pj59.c
+++ pj59.c
@@ -1,3 +1,53 @@
+#include "pool_map.h"
+#include "sector_list.h"
+#include "csv.h"
+#include "db.h"
+
 int main(int argc, char ** argv) {
-    return 0;
+    int status = 0;
+    struct pool list_node_pool;
+    struct pool map_node_pool;
+    struct pool sector_node_pool;
+    struct pool_map pool_map;
+    struct sector_list sector_list;
+    struct csv csv;
+    struct db db;
+
+    if(pool_create(&list_node_pool, sizeof(struct list_node), 64)) {
+        status = panic("failed to create pool object");
+    } else {
+        if(pool_create(&map_node_pool, sizeof(struct map_node), 64)) {
+            status = panic("failed to create pool object");
+        } else {
+            if(pool_create(&sector_node_pool, sizeof(struct sector_node), 64)) {
+                status = panic("failed to create pool object");
+            } else {
+                if(pool_map_create(&pool_map, 524288, &list_node_pool, &map_node_pool)) {
+                    status = panic("failed to create pool map object");
+                } else {
+                    if(sector_list_create(&sector_list, 524288, &sector_node_pool, &list_node_pool)) {
+                        status = panic("failed to create sector list object");
+                    } else {
+                        if(csv_create(&csv, 524288, &list_node_pool)) {
+                            status = panic("failed to create csv object");
+                        } else {
+                            if(db_create(&db, &pool_map, &sector_list, &csv)) {
+                                status = panic("failed to create db object");
+                            } else {
+                                db_destroy(&db);
+                            }
+                            csv_destroy(&csv);
+                        }
+                        sector_list_destroy(&sector_list);
+                    }
+                    pool_map_destroy(&pool_map);
+                }
+                pool_destroy(&sector_node_pool);
+            }
+            pool_destroy(&map_node_pool);
+        }
+        pool_destroy(&list_node_pool);
+    }
+
+    return status;
 }
