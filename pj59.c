21e3c7aad8debfe3c1f570361d8a51fda09f15bd
trickyloki3 <tricky.loki3@gmail.com>
2019-10-22 03:10:04 -0400

    * Updated pj59.

--- pj59.c
+++ pj59.c
@@ -3,6 +3,8 @@
 #include "db.h"
 #include "data.h"
 
+int load_test(struct pool_map *, struct sector_list *);
+
 int main(int argc, char ** argv) {
     int status = 0;
     struct pool list_node_pool;
@@ -10,10 +12,6 @@ int main(int argc, char ** argv) {
     struct pool sector_node_pool;
     struct pool_map pool_map;
     struct sector_list sector_list;
-    struct csv csv;
-    struct json json;
-    struct db db;
-    struct data data;
 
     if(argc < 2) {
         status = panic("missing working directory argument");
@@ -34,25 +32,8 @@ int main(int argc, char ** argv) {
                     if(sector_list_create(&sector_list, 524288, &sector_node_pool, &list_node_pool)) {
                         status = panic("failed to create sector list object");
                     } else {
-                        if(csv_create(&csv, 524288, &list_node_pool)) {
-                            status = panic("failed to create csv object");
-                        } else {
-                            if(json_create(&json, &pool_map, &sector_list)) {
-                                status = panic("failed to create json object");
-                            } else {
-                                if(db_create(&db, &pool_map, &sector_list, &csv, &json)) {
-                                    status = panic("failed to create db object");
-                                } else {
-                                    if(data_create(&data, &pool_map, &sector_list, &json)) {
-                                        status = panic("failed to create data object");
-                                    } else {
-                                        data_destroy(&data);
-                                    }
-                                    db_destroy(&db);
-                                }
-                                json_destroy(&json);
-                            }
-                            csv_destroy(&csv);
+                        if(load_test(&pool_map, &sector_list)) {
+                            status = panic("failed load test");
                         }
                         sector_list_destroy(&sector_list);
                     }
@@ -63,6 +44,37 @@ int main(int argc, char ** argv) {
             pool_destroy(&map_node_pool);
         }
         pool_destroy(&list_node_pool);
+    }
+
+    return status;
+}
+
+int load_test(struct pool_map * pool_map, struct sector_list * sector_list) {
+    int status = 0;
+    struct csv csv;
+    struct json json;
+    struct db db;
+    struct data data;
+
+    if(csv_create(&csv, 524288, pool_map_get(pool_map, sizeof(struct list_node)))) {
+        status = panic("failed to create csv object");
+    } else {
+        if(json_create(&json, pool_map, sector_list)) {
+            status = panic("failed to create json object");
+        } else {
+            if(db_create(&db, pool_map, sector_list, &csv, &json)) {
+                status = panic("failed to create db object");
+            } else {
+                if(data_create(&data, pool_map, sector_list, &json)) {
+                    status = panic("failed to create data object");
+                } else {
+                    data_destroy(&data);
+                }
+                db_destroy(&db);
+            }
+            json_destroy(&json);
+        }
+        csv_destroy(&csv);
     }
 
     return status;
