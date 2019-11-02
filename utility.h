b5c0a861ea195f60b1210e4b2d91710807991178
trickyloki3 <tricky.loki3@gmail.com>
2019-11-02 03:30:44 -0400

    * Included unistd.h in utility.

--- utility.h
+++ utility.h
@@ -8,6 +8,7 @@
 #include "stdlib.h"
 #include "stdint.h"
 #include "string.h"
+#include "unistd.h"
 
 #define panic(format, ...) panic_("%s (%s:%zu): " format ".\n", __FILE__, __func__, __LINE__, ## __VA_ARGS__)
 int panic_(const char *, ...);
