aaa28f2ca4e4cb9ec1b7745568abd8b8211f5b8b
trickyloki3 <tricky.loki3@gmail.com>
2019-12-20 22:33:49 -0500

    * Initial event callback for json.

--- json.c
+++ json.c
@@ -3,9 +3,26 @@
 #include "json_parser.h"
 #include "json_scanner.h"
 
-int json_parse_loop(yyscan_t, jsonpstate *);
+int json_parse_loop(struct json *, yyscan_t, jsonpstate *);
 
-int json_parse(const char * path, size_t size) {
+int json_create(struct json * json, size_t size) {
+    int status = 0;
+
+    if(strbuf_create(&json->strbuf, size)) {
+        status = panic("failed to create strbuf object");
+    } else {
+        json->token = 0;
+        json->string = NULL;
+    }
+
+    return status;
+}
+
+void json_destroy(struct json * json) {
+    strbuf_destroy(&json->strbuf);
+}
+
+int json_parse(struct json * json, const char * path, size_t size, event_cb callback, void * context) {
     int status = 0;
 
     FILE * file;
@@ -13,11 +30,14 @@ int json_parse(const char * path, size_t size) {
     jsonpstate * parser;
     YY_BUFFER_STATE buffer;
 
+    json->callback = callback;
+    json->context = context;
+
     file = fopen(path, "r");
     if(!file) {
         status = panic("failed to open %s", path);
     } else {
-        if(jsonlex_init_extra(NULL, &scanner)) {
+        if(jsonlex_init_extra(json, &scanner)) {
             status = panic("failed to create scanner object");
         } else {
             parser = jsonpstate_new();
@@ -29,7 +49,7 @@ int json_parse(const char * path, size_t size) {
                     status = panic("faield to create buffer state object");
                 } else {
                     jsonpush_buffer_state(buffer, scanner);
-                    if(json_parse_loop(scanner, parser))
+                    if(json_parse_loop(json, scanner, parser))
                         status = panic("failed to parse loop json object");
                     jsonpop_buffer_state(scanner);
                 }
@@ -43,7 +63,7 @@ int json_parse(const char * path, size_t size) {
     return status;
 }
 
-int json_parse_loop(yyscan_t scanner, jsonpstate * parser) {
+int json_parse_loop(struct json * json, yyscan_t scanner, jsonpstate * parser) {
     int status = 0;
 
     JSONSTYPE value;
@@ -61,6 +81,52 @@ int json_parse_loop(yyscan_t scanner, jsonpstate * parser) {
                 status = panic("failed to parse the current token");
         }
     } while(token && state == YYPUSH_MORE && !status);
+
+    if(status) {
+        /* skip the last token on error */
+    } else if(json_token(json, 0, NULL, 0)) {
+        status = panic("failed to token json object");
+    }
+
+    /*
+     * reset json object to initial state
+     */
+    json->string = NULL;
+    json->token = 0;
+    strbuf_clear(&json->strbuf);
+
+    return status;
+}
+
+int json_token(struct json * json, int token, char * string, size_t length) {
+    int status = 0;
+
+    /*
+     * events are generated from scanner, but
+     * takes into account the lookahead token
+     */
+    if(json->token) {
+        if(json->callback(json->token, json->string, json->context))
+            status = panic("failed to process event");
+        strbuf_clear(&json->strbuf);
+    }
+
+    if(status) {
+        /* skip the next token on error */
+    } else {
+        json->token = token;
+        if(string) {
+            if(strbuf_strcpy(&json->strbuf, string, length)) {
+                status = panic("failed to strcpy strbuf object");
+            } else {
+                json->string = strbuf_string(&json->strbuf);
+                if(!json->string)
+                    status = panic("failed to string strbuf object");
+            }
+        } else {
+            json->string = NULL;
+        }
+    }
 
     return status;
 }
--- json.h
+++ json.h
@@ -1,8 +1,19 @@
 #ifndef json_h
 #define json_h
 
-#include "utility.h"
+#include "event.h"
 
-int json_parse(const char *, size_t);
+struct json {
+    struct strbuf strbuf;
+    int token;
+    struct string * string;
+    event_cb callback;
+    void * context;
+};
+
+int json_create(struct json *, size_t);
+void json_destroy(struct json *);
+int json_parse(struct json *, const char *, size_t, event_cb, void *);
+int json_token(struct json *, int, char *, size_t);
 
 #endif
