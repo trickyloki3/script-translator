2562672fe6c05a324b07be1595ba9e45026a7d52
trickyloki3 <tricky.loki3@gmail.com>
2019-12-25 15:21:36 -0500

    * Revised list.

--- list.c
+++ list.c
@@ -1,33 +1,25 @@
 #include "list.h"
 
-int list_node_create(struct list *, void *, struct list_node **);
-void list_node_destroy(struct list *, struct list_node *);
+static inline struct list_node * list_node_create(struct list *, void *);
+static inline void list_node_destroy(struct list *, struct list_node *);
 static inline void list_node_attach(struct list_node *, struct list_node *);
 static inline void list_node_detach(struct list_node *);
 
-int list_node_create(struct list * list, void * object, struct list_node ** result) {
-    int status = 0;
+static inline struct list_node * list_node_create(struct list * list, void * object) {
     struct list_node * node;
 
-    node = list->pool ? pool_get(list->pool) : malloc(sizeof(*node));
-    if(!node) {
-        status = panic("out of memory");
-    } else {
+    node = pool_get(list->pool);
+    if(node) {
         node->object = object;
         node->next = node;
         node->prev = node;
-        *result = node;
     }
 
-    return status;
+    return node;
 }
 
-void list_node_destroy(struct list * list, struct list_node * node) {
-    if(list->pool) {
-        pool_put(list->pool, node);
-    } else {
-        free(node);
-    }
+static inline void list_node_destroy(struct list * list, struct list_node * node) {
+    pool_put(list->pool, node);
 }
 
 static inline void list_node_attach(struct list_node * x, struct list_node * y) {
@@ -47,8 +39,8 @@ static inline void list_node_detach(struct list_node * x) {
 int list_create(struct list * list, struct pool * pool) {
     int status = 0;
 
-    if(pool && pool->size != sizeof(struct list_node)) {
-        status = panic("pool is invalid");
+    if(!pool || pool->size < sizeof(struct list_node)) {
+        status = panic("invalid pool");
     } else {
         list->pool = pool;
         list->root = NULL;
@@ -59,17 +51,7 @@ int list_create(struct list * list, struct pool * pool) {
 }
 
 void list_destroy(struct list * list) {
-    struct list_node * node;
-
-    if(list->root) {
-        while(list->root != list->root->next) {
-            node = list->root->next;
-            list_node_detach(node);
-            list_node_destroy(list, node);
-        }
-        list_node_destroy(list, list->root);
-        list->root = NULL;
-    }
+    while(list_pop(list));
     list->iter = NULL;
 }
 
@@ -77,40 +59,21 @@ void list_clear(struct list * list) {
     list_destroy(list);
 }
 
-int list_copy(struct list * result, struct list * copy) {
-    int status = 0;
-    void * object;
-
-    if(list_create(result, copy->pool)) {
-        status = panic("failed to create list object");
-    } else {
-        object = list_start(copy);
-        while(object && !status) {
-            if(list_push(result, object))
-                status = panic("failed to push list object");
-            object = list_next(copy);
-        }
-
-        if(status)
-            list_destroy(result);
-    }
-
-    return status;
-}
-
 int list_push(struct list * list, void * object) {
     int status = 0;
     struct list_node * node;
 
     if(!object) {
-        status = panic("object is zero");
-    } else if(list_node_create(list, object, &node)) {
-        status = panic("failed to create node object");
+        status = panic("invalid object");
     } else {
-        if(list->root)
-            list_node_attach(list->root, node);
-
-        list->root = node;
+        node = list_node_create(list, object);
+        if(!node) {
+            status = panic("failed to create node object");
+        } else {
+            if(list->root)
+                list_node_attach(list->root, node);
+            list->root = node;
+        }
     }
 
     return status;
@@ -121,10 +84,9 @@ void * list_pop(struct list * list) {
     struct list_node * node;
 
     if(list->root) {
-        object = list->root->object;
-
         node = list->root;
         list->root = (list->root == list->root->prev) ? NULL : list->root->prev;
+        object = node->object;
         list_node_detach(node);
         list_node_destroy(list, node);
     }
--- list.h
+++ list.h
@@ -18,7 +18,6 @@ struct list {
 int list_create(struct list *, struct pool *);
 void list_destroy(struct list *);
 void list_clear(struct list *);
-int list_copy(struct list *, struct list *);
 int list_push(struct list *, void *);
 void * list_pop(struct list *);
 void * list_top(struct list *);
