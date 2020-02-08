11fd0459d4a38d0e0d61ab32ca643270cf0ffec3
trickyloki3 <tricky.loki3@gmail.com>
2020-02-07 23:19:04 -0500

    * Revised misc.

--- misc.c
+++ misc.c
@@ -6,21 +6,41 @@ int long_compare(void * x, void * y) {
     return l < r ? -1 : l > r ? 1 : 0;
 }
 
+struct long_array * long_array_create(struct store * store, size_t count) {
+    int status = 0;
+    struct long_array * array;
+
+    array = store_object(store, sizeof(*array));
+    if(!array) {
+        status = panic("failed to object store object");
+    } else {
+        array->count = count;
+        array->array = store_object(store, sizeof(*array->array) * array->count);
+        if(!array->array)
+            status = panic("failed to object store object");
+    }
+
+    return status ? NULL : array;
+}
+
 int string_store(struct string * string, struct store * store, struct string ** result) {
     int status = 0;
     struct string * object;
 
-    object = store_object(store, sizeof(*string) + string->length + 1);
+    object = store_object(store, sizeof(*object));
     if(!object) {
         status = panic("failed to object store object");
     } else {
-        object->string = (char *) object + sizeof(*string);
         object->length = string->length;
+        object->string = store_object(store, sizeof(string->string) * (string->length + 1));
+        if(!object->string) {
+            status = panic("failed to object store object");
+        } else {
+            memcpy(object->string, string->string, string->length);
+            object->string[object->length] = 0;
 
-        memcpy(object->string, string->string, string->length);
-        object->string[object->length] = 0;
-
-        *result = object;
+            *result = object;
+        }
     }
 
     return status;
@@ -32,15 +52,11 @@ int string_strtol(struct string * string, long * result) {
     long value;
     char * end;
 
-    if(!string->length) {
-        *result = 0;
+    value = strtol(string->string, &end, 0);
+    if(*end) {
+        status = panic("invalid '%s' in '%s'", end, string->string);
     } else {
-        value = strtol(string->string, &end, 0);
-        if(string->string + string->length != end) {
-            status = panic("invalid '%s' in '%s'", end, string->string);
-        } else {
-            *result = value;
-        }
+        *result = value;
     }
 
     return status;
@@ -63,29 +79,22 @@ int string_strtol_split(struct string * string, char split, struct store * store
             ptr++;
     }
 
-    array = store_object(store, sizeof(*array));
+    array = long_array_create(store, count);
     if(!array) {
-        status = panic("failed to object store object");
+        status = panic("failed to create long array object");
     } else {
-        array->count = count;
-        array->array = store_object(store, sizeof(*array->array) * array->count);
-        if(!array->array) {
-            status = panic("failed to object store object");
+        ptr = string->string;
+        count = 0;
+        while(ptr && count < array->count) {
+            array->array[count++] = strtol(ptr, &end, 0);
+            ptr = *end == split ? end + 1 : NULL;
+            if(!ptr && *end)
+                status = panic("invalid string '%s' in '%s'", end, string->string);
+        }
+        if(status) {
+            *result = NULL;
         } else {
-            ptr = string->string;
-            count = 0;
-            while(ptr && count < array->count) {
-                array->array[count++] = strtol(ptr, &end, 0);
-                ptr = *end == split ? end + 1 : NULL;
-                if(!ptr && *end)
-                    status = panic("invalid string '%s' in '%s'", end, string->string);
-            }
-
-            if(status) {
-
-            } else {
-                *result = array;
-            }
+            *result = array;
         }
     }
 
@@ -104,12 +113,10 @@ int string_strtol_splitv(struct string * string, int split, ...) {
     ptr = string->string;
     while(value && ptr) {
         *value = strtol(ptr, &end, 0);
+        value = va_arg(args, long *);
         ptr = *end == split ? end + 1 : NULL;
-        if(!ptr && *end) {
+        if(!ptr && *end)
             status = panic("invalid string '%s' in '%s'", end, string->string);
-        } else {
-            value = va_arg(args, long *);
-        }
     }
     va_end(args);
 
