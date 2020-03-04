6792b3e4a1d0ca226518bbcf397b2691a06052b2
trickyloki3 <tricky.loki3@gmail.com>
2020-03-03 23:47:22 -0500

    * Revised pj59.

--- pj59.c
+++ pj59.c
@@ -10,32 +10,30 @@ int main(int argc, char ** argv) {
 
     if(chdir(argv[1])) {
         status = panic("failed to change directory");
+    } else if(heap_create(&heap, 4096)) {
+        status = panic("failed to create heap object");
     } else {
-        if(heap_create(&heap, 4096)) {
-            status = panic("failed to create heap object");
+        if(table_create(&table, 65536, &heap)) {
+            status = panic("failed to create table object");
         } else {
-            if(table_create(&table, 65536, &heap)) {
-                status = panic("failed to create table object");
+            if(table_item_parse(&table, "item_db.txt")) {
+                status = panic("failed to item parse table object");
             } else {
-                if(table_item_parse(&table, "item_db.txt")) {
-                    status = panic("failed to item parse table object");
-                } else {
-                    if(argc < 3) {
-                        item = item_start(&table);
-                        while(item) {
+                if(argc < 3) {
+                    item = item_start(&table);
+                    while(item && !status) {
 
-                            item = item_next(&table);
-                        }
-                    } else {
-                        item = item_id(&table, strtol(argv[2], NULL, 0));
-                        if(!item)
-                            status = panic("invalid item id - %s", argv[2]);
+                        item = item_next(&table);
                     }
+                } else {
+                    item = item_id(&table, strtol(argv[2], NULL, 0));
+                    if(!item)
+                        status = panic("invalid item id - %s", argv[2]);
                 }
-                table_destroy(&table);
             }
-            heap_destroy(&heap);
+            table_destroy(&table);
         }
+        heap_destroy(&heap);
     }
 
     return status;
