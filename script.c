7ba10c2c9c34cb6bb35884368551e8ef1213a154
trickyloki3 <tricky.loki3@gmail.com>
2020-03-05 01:09:12 -0500

    * Optimized script_parse_loop.

--- script.c
+++ script.c
@@ -85,26 +85,29 @@ int script_parse(struct script * script, char * string) {
 int script_parse_loop(struct script * script, struct string * string) {
     int status = 0;
 
+    yyscan_t scanner = script->scanner;
+    scriptpstate * parser = script->parser;
+
     YY_BUFFER_STATE buffer;
     SCRIPTSTYPE value;
     int token;
     int state = YYPUSH_MORE;
 
-    buffer = script_scan_buffer(string->string, string->length, script->scanner);
+    buffer = script_scan_buffer(string->string, string->length, scanner);
     if(!buffer) {
         status = panic("failed to scan buffer scanner object");
     } else {
         while(state == YYPUSH_MORE && !status) {
-            token = scriptlex(&value, script->scanner);
+            token = scriptlex(&value, scanner);
             if(token < 0) {
                 status = panic("failed to get the next token");
             } else {
-                state = scriptpush_parse(script->parser, token, &value, script);
+                state = scriptpush_parse(parser, token, &value, script);
                 if(state && state != YYPUSH_MORE)
                     status = panic("failed to parse the current token");
             }
         }
-        scriptpop_buffer_state(script->scanner);
+        scriptpop_buffer_state(scanner);
     }
 
     return status;
